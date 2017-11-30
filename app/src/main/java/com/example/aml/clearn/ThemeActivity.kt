a3017e5ace651f5bb90ee47d866ed8ad38dfdb8b
xvhuichuang <519401502@qq.com>
2017-11-30 17:21:21 +0800

    补间动画的使用

--- app/src/main/java/com/example/aml/clearn/ThemeActivity.kt
+++ app/src/main/java/com/example/aml/clearn/ThemeActivity.kt
@@ -1,15 +1,19 @@
 package com.example.aml.clearn
 
+import android.animation.ObjectAnimator
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
+import android.widget.ImageView
 
 import kotlinx.android.synthetic.main.activity_theme.*
 
 class ThemeActivity : AppCompatActivity() {
 
     lateinit private var mButton: Button
+    private lateinit var mImageView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +23,7 @@ class ThemeActivity : AppCompatActivity() {
             setTheme(R.style.AppTheme_Night)
             recreate()
         }
+
     }
 
 }
